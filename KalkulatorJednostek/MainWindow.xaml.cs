ce2f3c850e9958e6ba9244313ed17696ad4ba06d
Jakub Piasecki <jakkubpiasecki87@gmail.com>
2022-02-03 16:30:38 +0100

    initial commit

---
+++ KalkulatorJednostek/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnitConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Unit> units = new List<Unit>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            units.AddRange(Methods.AddUnits());
+
+            SourceUnitList.ItemsSource = units;
+            OutputUnitList.ItemsSource= units;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var From = units[SourceUnitList.SelectedIndex];
+            From.Ammount = double.Parse(Input.Text);
+            var To = units[OutputUnitList.SelectedIndex];
+            Methods.Calculate(From,To);
+            Output.Text=""+To.Ammount;
+        }
+    }
+
+
+
+    
+}
---
+++ KalkulatorJednostek/Methods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter
+{
+    public class Methods
+    {
+        public static IEnumerable<Unit> AddUnits()
+        {
+            List<Unit> units= new List<Unit>();
+                
+            units.Add(new Unit("Meters"));
+            units.Add(new Unit("Feet"));
+            units.Add(new Unit("Miles"));
+            units.Add(new Unit("Kilometer"));
+
+            return units;
+        }
+
+        public static void Calculate(Unit from, Unit to)
+        {
+            double inMeters = 0;
+
+            switch (from.Name)
+            {
+                case "Meters":
+                    inMeters = from.Ammount;
+                    break;
+                case "Feet":
+                    inMeters = from.Ammount * 0.3048;
+                    break;
+                case "Miles":
+                    inMeters=from.Ammount * 1609.344;
+                    break;
+                case "Kilometer":
+                    inMeters = from.Ammount * 1000;
+                    break;
+            }
+
+            switch (to.Name)
+            {
+                case "Meters":
+                    to.Ammount = inMeters;
+                    break;
+                case "Feet":
+                    to.Ammount = inMeters * 3.2808399;
+                    break;
+                case "Miles":
+                    to.Ammount = inMeters * 0.000621371192;
+                    break;
+                case "Kilometer":
+                    to.Ammount = inMeters / 1000;
+                    break;
+            }
+
+        }
+    }
+}
